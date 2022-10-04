b6e79aebf28bf69e545a41b083f65542ef5d2a45
Kruceo <rafa2015krueger@gmail.com>
2022-10-04 19:25:04 -0300

    fix: export obj

---
+++ readme.md
@@ -0,0 +1,12 @@
+**To init:**
+```js
+js const c = require('coloral');
+```
+
+**To use:**
+
+```js
+console.log(c.red("I'm red!"))
+```
+
+ 
---
+++ teste/test.js
@@ -0,0 +1,6 @@
+const c = require('../index');
+
+console.log(c.all.markred(' ERROR ') + c.all.red(' erro encontrado!'));
+console.log(c.all.markblue(' SERVER ') + c.all.blue(' 198.168.0.110 entrou no server...'));
+console.log(c.all.markred(' ERROR ') + c.all.red(' erro encontrado!'));
+console.log(c.all.markred(' ERROR ') + c.all.red(' erro encontrado!'));
